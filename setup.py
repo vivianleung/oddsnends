7c80ca65576251cd73d2c8a7a438892708c2e884
Vivian Leung <vivianleung19@gmail.com>
2023-10-16 21:53:47 +0800

    changed version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         # ],
     },
     install_requires=requirements,
-    license="MIT license",
+    license="MIT License",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='oddsnends',
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vivianleung/oddsnends',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
