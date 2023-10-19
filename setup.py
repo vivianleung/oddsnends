a07e378575e43fc39463b66d83dea1622ed5dd55
Vivian Leung <vivianleung19@gmail.com>
2023-10-19 15:31:14 +0800

    changed version number

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vivianleung/oddsnends',
-    version='0.1.1dev0',
+    version='0.1.1dev1',
     zip_safe=False,
 )
