e5b3894cd9eb919e3b06a85864b692eddd72a08c
Vivian Leung <leung.vivian.w@gmail.com>
2023-11-13 09:59:13 +0800

    added find_fpaths and read_pkl_gz funcs

--- oddsnends/ioa.py
+++ oddsnends/ioa.py
@@ -3,13 +3,15 @@
 # Input, output and args-related
 import os
 from argparse import ArgumentParser
-from typing import Annotated, Any
+from colllections.abc import Callable, Generator
+from typing import Annotated
 
 __all__ = [
     "LoggingLevels",
     "assertfile",
     "assertexists",
     "filepath",
+    "find_fpaths",
 ]
 
 LoggingLevels = {
@@ -21,30 +23,35 @@ LoggingLevels = {
     "CRITICAL": 50,
 }
 
-def assertfile(fpath: str | None, *err_args,
-               null: Annotated[str, "ignore", "raise"] = "ignore") -> None:
+
+def assertfile(
+    fpath: str | None, *err_args, null: Annotated[str, "ignore", "raise"] = "ignore"
+) -> None:
     """Asserts isfile. Raises FileNotFoundError only if fpath is non-null."""
     try:
         assert os.path.isfile(fpath)
-    except AssertionError:
-        raise FileNotFoundError(fpath, *err_args)
-    except TypeError:
+    except AssertionError as error:
+        raise FileNotFoundError(fpath, *err_args) from error
+    except TypeError as error:
         if (fpath is not None) or (null == "raise"):
-            raise TypeError(fpath, type(fpath))
+            raise TypeError(fpath, type(fpath)) from error
 
-def assertexists(fpath: str | None, *err_args,
-               null: Annotated[str, "ignore", "raise"] = "ignore") -> None:
+
+def assertexists(
+    fpath: str | None, *err_args, null: Annotated[str, "ignore", "raise"] = "ignore"
+) -> None:
     """Asserts exists. Raises FileNotFoundError only if fpath is non-null."""
     try:
         assert os.path.exists(fpath)
-    except AssertionError:
-        raise FileNotFoundError(fpath, *err_args)
-    except TypeError:
+    except AssertionError as error:
+        raise FileNotFoundError(fpath, *err_args) from error
+    except TypeError as error:
         if (fpath is not None) or (null == "raise"):
-            raise TypeError(fpath, type(fpath))
+            raise TypeError(fpath, type(fpath)) from error
+
 
 def filepath(parser: ArgumentParser, arg: str | None) -> str:
-    """Check if arg given to parser is a valid file, raising ArgumentError 
+    """Check if arg given to parser is a valid file, raising ArgumentError
     otherwise
     """
     arg = arg.strip()
@@ -53,3 +60,68 @@ def filepath(parser: ArgumentParser, arg: str | None) -> str:
     except (AssertionError, TypeError):
         parser.error(f"file does not exist: {arg}")
     return arg
+
+
+def find_fpaths(
+    rootdir: str,
+    suffix: str = None,
+    prefix: str = None,
+    filter_fn: Callable = None,
+    filter_kws: dict = None,
+    **kws
+) -> Generator[tuple[str, str]]:
+    """Returns filenames and paths in root directory
+    Parameters
+    ----------
+    rootdir:  str
+        Directory to search
+    suffix: str, optional
+        Filter by suffix. Can use in combination with prefix. Default None
+    prefix:
+        Filter by prefix. Can use in combination with prefix. Default None
+    filter_fn: functionn, optional
+        Filter files. If given, the function should take the file name and dir
+        as the first two positional arguments, and return a bool. suffix and 
+        prefix args are ignored if this is given.
+    filter_kws: dict, optional
+        Passed to filter_fn
+    **kws
+        Passed to os.walk
+    
+    Returns
+    -------
+    Generator of (filename, filepath)
+    """
+    # defaults
+    if filter_kws is None:
+        filter_kws = {}
+    
+    # check/construct filter function
+    if filter_fn is not None:
+        pass
+
+    elif (suffix is not None) and (prefix is not None):
+        filter_fn = lambda s: s.startswith(prefix) and s.endswith(prefix)
+
+    elif suffix is not None:
+        filter_fn = lambda s: s.endswith(suffix)
+
+    elif prefix is not None:
+        filter_fn = lambda s: s.startswith(prefix)
+
+    else:  # no filter
+        filter_fn = lambda _: True
+
+    # walk
+    return (
+        (fname, f"{d}/{fname}")
+        for d, _, files in os.walk(rootdir, **kws)
+        for fname in files
+        if filter_fn(fname)
+    )
+
+
+def read_pkl_gz(fpath: str):
+    """Read in data from a gzipped pickle"""
+    with gzip.open(fpath, "rb") as fin:
+        return pickle.load(fin)
